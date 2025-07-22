b3719b3260906ffe5275054288ad5da50adbda0c
TeeshaGuptaNGTS <ngtsteesha@gmail.com>
2025-07-22 13:19:40 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 "use client";
-import Image from "next/image";
-import Doctor from "./medical-specialist/page";
 import SpecialityClinics from "./speciality-clinics/page";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        {/* <Doctor/> */}
         <SpecialityClinics/>
       </main>
     
